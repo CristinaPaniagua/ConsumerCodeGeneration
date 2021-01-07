f01cdde06ffb89feb2964075e6a968714ccde043
B2413\cripan-local <cristina.paniagua@ltu.se>
2021-01-07 13:22:31 +0100

    interface provider new

---
+++ ConsumerGenerationModulesSpring/Interface_provider/src/main/java/ai/aitia/demo/car_provider/controller/Arguments.java
@@ -0,0 +1,40 @@
+// Auto generated
+package ai.aitia.demo.car_provider.controller;
+
+import java.lang.Override;
+import java.lang.String;
+
+public class Arguments {
+  private String name;
+
+  private String value;
+
+  public Arguments() {
+  }
+
+  public Arguments(String name, String value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return "ProviderPayload{" + "name=" + name+ "value=" + value +"}";
+  }
+
+  public String getname() {
+    return name;
+  }
+
+  public void setname(String name) {
+    this.name=name;
+  }
+
+  public String getvalue() {
+    return value;
+  }
+
+  public void setvalue(String value) {
+    this.value=value;
+  }
+}
---
+++ ConsumerGenerationModulesSpring/Interface_provider/src/main/java/ai/aitia/demo/car_provider/controller/RequestDTO.java
@@ -0,0 +1,67 @@
+// Auto generated
+package ai.aitia.demo.car_provider.controller;
+
+import java.lang.Integer;
+import java.lang.Override;
+import java.lang.String;
+
+public class RequestDTO {
+  private Integer SessionId;
+
+  private String OfferorName;
+
+  private String RecieverName;
+
+  private Contracts Contracts;
+
+  public RequestDTO() {
+  }
+
+  public RequestDTO(Integer SessionId, String OfferorName, String RecieverName,
+      Contracts Contracts) {
+    this.SessionId = SessionId;
+    this.OfferorName = OfferorName;
+    this.RecieverName = RecieverName;
+    this.Contracts = Contracts;
+  }
+
+  @Override
+  public String toString() {
+    return "ProviderPayload{" + "SessionId=" + SessionId+ "OfferorName=" + OfferorName+ "RecieverName=" + RecieverName+ "Contracts=" + Contracts +"}";
+  }
+
+  public Integer getSessionId() {
+    return SessionId;
+  }
+
+  public void setSessionId(Integer SessionId) {
+    this.SessionId=SessionId;
+  }
+
+  public String getOfferorName() {
+    return OfferorName;
+  }
+
+  public void setOfferorName(String OfferorName) {
+    this.OfferorName=OfferorName;
+  }
+
+  public String getRecieverName() {
+    return RecieverName;
+  }
+
+  public void setRecieverName(String RecieverName) {
+    this.RecieverName=RecieverName;
+  }
+
+  public Contracts getContracts() {
+    return Contracts;
+  }
+
+  public void setContracts(Contracts Contracts) {
+    this.Contracts=Contracts;
+  }
+  
+  
+  
+}
